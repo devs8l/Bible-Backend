8126a4381b24b345356a37358fa3daac57e8bcdf
devs8l <developer.stellar8@gmail.com>
2025-07-05 17:40:23 +0530

    fixing commit

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -4,8 +4,9 @@ import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router()
 
-cartRouter.post("/get",authUser, getUserCart)
 cartRouter.post("/add",authUser, addToCart)
 cartRouter.post("/update", authUser, updateCart)
+cartRouter.post("/get",authUser, getUserCart)
+
 
 export default cartRouter;
