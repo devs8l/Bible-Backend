f0c29e3df76639417336de31feb6fe92ad744b4e
devs8l <developer.stellar8@gmail.com>
2025-07-21 18:35:25 +0530

    date 21 commit

--- utils/sendDigitalCopy.js
+++ utils/sendDigitalCopy.js
@@ -19,7 +19,7 @@ const sendDigitalCopy = async (email, name, digitalFileUrl, productName) => {
         <div style="font-family: sans-serif; color: #333;">
           <h2>Hi ${name},</h2>
           <p>Thank you for purchasing <strong>${productName}</strong> from Bible Store.</p>
-          <p>Your digital copy is attached below. You can also <a href="${digitalFileUrl}">download it directly here</a>.</p>
+          <p>Your digital copy is attached below.</p>
           <br/>
           <p>Blessings,</p>
           <p><strong>Bible Store Team</strong></p>
