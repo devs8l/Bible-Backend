3844f9f147741354a8272f035f3effe0144cdb57
devs8l <developer.stellar8@gmail.com>
2025-10-30 18:21:04 +0530

    30/10/2025 Add Payment Toggle models

--- routes/paymentSettingsRoutes.js
+++ routes/paymentSettingsRoutes.js
@@ -5,7 +5,7 @@ import { verifyAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Get current settings
-router.get("/", verifyAdmin, getPaymentSettings);
+router.get("/", getPaymentSettings);
 
 // Update (Admin only)
 router.put("/", verifyAdmin, updatePaymentSettings);
